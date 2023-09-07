72d21dc0a47773e724310d084b249f9bc1533edb
JyTey2004 <tey.jia.ye@gmail.com>
2023-09-07 19:19:53 +0800

    Added the shopping screen

--- frontend/screens/ServicesScreen.js
+++ frontend/screens/ServicesScreen.js
@@ -26,6 +26,46 @@ const ServicesScreen = () => {
     // Add more content creators as needed
   ];
 
+  const sellingItems = [
+    {
+      id: 1,
+      brand: 'Nike',
+      name: 'Air Jordan 1',
+      price: 1000,
+      image: 'https://wallpapers.com/images/high/cool-profile-picture-1ecoo30f26bkr14o.webp',
+    },
+    {
+      id: 2,
+      brand: 'Adidas',
+      name: 'Yeezy 350',
+      price: 1000,
+      image: 'https://wallpapers.com/images/high/cool-profile-picture-1ecoo30f26bkr14o.webp',
+    },
+    {
+      id: 3,
+      brand: 'Nike',
+      name: 'Peaceminusone Air Force 1',
+      price: 1000,
+      image: 'https://wallpapers.com/images/high/cool-profile-picture-1ecoo30f26bkr14o.webp',
+    },
+    {
+      id: 4,
+      brand: 'Nike',
+      name: 'Air Jordan 1',
+      price: 1000,
+      image: 'https://wallpapers.com/images/high/cool-profile-picture-1ecoo30f26bkr14o.webp',
+    }
+  ]
+
+
+  const pairs = [];
+  for (let i = 0; i < sellingItems.length; i += 2) {
+    pairs.push({
+      item1: sellingItems[i],
+      item2: sellingItems[i + 1],
+    });
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
         <View style={{marginHorizontal: '3%'}}>
@@ -56,12 +96,41 @@ const ServicesScreen = () => {
             </View>
 
             {/* Content Creators */}
-            <ScrollView style={{ height: '15%', marginTop: '3%'}} horizontal>
+            <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold', marginTop: '3%' }}>Content Creators</Text>
+            <ScrollView style={{ height: '20%', marginTop: '3%'}} horizontal>
                 {contentCreators.map((creator) => (
-                    <View key={creator.id} style={{ alignItems: 'center', flex: 1}}>
-                        <Image source={{uri: creator.profileImage}} style={{width: 100, height: 100, borderRadius: '30%', borderColor: '#ef4444', borderWidth: '1%' }} />
-                        <Text style={{ marginLeft: 10, fontWeight: 'bold', color: 'white' }} className='text-sm'>@{creator.name}</Text>
+                    <View key={creator.id} style={{ width: 100, height: '100%', padding: 3 }} className='items-center'>
+                        <Image source={{uri: creator.profileImage}} style={{width: '100%', height: '80%', borderRadius: 30 }} />
+                        <Text style={{ marginLeft: 10, fontWeight: 'semibold', color: 'white' }} className='text-sm'>{creator.name}</Text>
                     </View>
+                ))}
+            </ScrollView>
+
+            {/* Selling Items */}
+            <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold', marginTop: '3%' }}>Trending</Text>
+            <ScrollView style={{ marginTop: '3%'}} className='w-max h-max'>
+                {pairs.map((pair) => (
+                  <View key={pair.item1.id} style={{ flex: 1, flexDirection: 'row' }}>
+                    <View style={{ flex: 1, padding: 3}}>
+                      <Image source={{uri: pair.item1.image}} style={{width: 150, height: 200, borderRadius: 30 }} />
+                      <Text style={{ marginLeft: 10, fontWeight: 'bold', color: 'white' }} className='text-sm'>{pair.item1.brand}</Text>
+                      <Text style={{ marginLeft: 10, fontWeight: 'bold', color: 'white' }} className='text-sm'>{pair.item1.name}</Text>
+                      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Text style={{ marginLeft: 10, fontWeight: 'bold', color: 'white' }} className='text-sm'>Price</Text>
+                        <Text style={{ marginLeft: 10, fontWeight: 'bold', color: 'white' }} className='text-sm'>${pair.item1.price}</Text>
+                      </View>
+                    </View>
+                    <View style={{ flex: 1, padding: 3}}>
+                      <Image source={{uri: pair.item2.image}} style={{width: 150, height: 200, borderRadius: 30 }} />
+                      <Text style={{ marginLeft: 10, fontWeight: 'bold', color: 'white' }} className='text-sm'>{pair.item2.brand}</Text>
+                      <Text style={{ marginLeft: 10, fontWeight: 'bold', color: 'white' }} className='text-sm'>{pair.item2.name}</Text>
+                      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Text style={{ marginLeft: 10, fontWeight: 'bold', color: 'white' }} className='text-sm'>Price</Text>
+                        <Text style={{ marginLeft: 10, fontWeight: 'bold', color: 'white' }} className='text-sm'>${pair.item2.price}</Text>
+                      </View>
+                    </View>
+
+                  </View>
                 ))}
             </ScrollView>
         </View>
